b3ced0a595c47043644c2c6d12e917f69e5b6021
Jabier Espinal <jabierespinal@gmail.com>
2017-11-23 05:27:54 +0000

    dynamic dropdownv2

--- script.js
+++ script.js
@@ -1,31 +1,62 @@
 var cDealer = { 
-    name: 'Enterprize Car Rentals',  
-    Midsize: {
+    cars: [  
+    {
+     name: "Midsize",
       price:500,
       available:31
     },    
-    Economy: {
+    {
+    name: "Economy Suv", 
+    price: 500,
+    available: 40
+    },
+    {
+    name: "Economy Sedan",
       price:400,
       available:25
     },
-                                                                           
-    Tesla: {
+    {                                                                       
+    name: "Tesla roadster",
       price:1000,
       available:10
-    }
+    } 
+
+            ],
+    
+    name: 'Enterprize Car Rentals'
     };
   
-var uRenter = {
+    var uRenter = {
     
-        Economy:[],
         Midsize:[],
+        EconomySuv:[],
+        EconomySedan:[],
         Tesla:[]
+        
     };
     
 function Info(val){
     var selection = cDealer[val];
     document.getElementById('Info').innerHTML = val + " $" + selection.price + "<br>" + selection.available + " Available";
 }    
+    for (var i = 0; i < cDealer.cars.length; i++){
+    var option = document.createElement("OPTION");
+    var carTp = document.createElement("LABEL");
+    document.getElementById("carSelect").appendChild(option);
+    //option.setAttribute("type", "option");
+    option.setAttribute("name", "cars"); 
+    option.setAttribute("value", i); 
+    option.setAttribute("id", "car" + i); 
+    option.setAttribute("onclick", "myFunction(" + i + ")"); 
+    carTp.innerHTML = cDealer.cars[i].name; 
+    option.innerHTML = cDealer.cars[i].name; 
+    
+    document.getElementById("carSelect").appendChild(option);
+    //document.getElementById("carSelect").appendChild(carTp);
+}
+    document.getElementById("reserver").onsubmit = function(event) {
+    event.preventDefault();
+    
     function rent(){
         
         var size = document.forms["carSelect"]['type'].value;
